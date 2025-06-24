ef3b76cc36e28f1c4e28b934417cca09894af36d
Ceorl <dev.rocher.florian@gmail.com>
2025-06-24 16:51:34 +0200

    mise en place fonction initialisation DB et formatage

--- utils/database.py
+++ utils/database.py
@@ -1,16 +1,46 @@
+import json
+import os
 from datetime import date
 
 from tinydb import TinyDB
 from models.joueur import Joueur
 
-def initialisation_db_joueur():
-    path = "../data/joueurs/joueurs.json"
-    return TinyDB(path)
+PATH_JOUEURS ="../data/joueurs/joueurs.json"
+PATH_TOURNOIS ="../data/tournaments/tournois.json"
 
-def initialisation_db_tournoi():
-    path = "../data/joueurs/tournois.json"
-    return TinyDB(path)
+
+def initialisation_db(path):
+    """
+    Vérifie l'existance du dossier et du fichier .json
+    :param path:
+    :return:
+    """
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+
+    if not os.path.exists(path) or os.stat(path).st_size == 0:
+        with open(path, "w", encoding="utf-8") as f:
+            json.dump({}, f)  # initialise avec un objet vide
+
+
+def format_db(path: str):
+    """
+    formate la tinyDB pour une version plus lisible
+    :param path: chemin relatif vers le fichier .json
+    :return: None
+    """
+    with open(path, "r", encoding="utf-8") as f:
+        content = f.read()
+
+    if not content.strip():  # fichier vide
+        return f"Fichier vide : {path}, on saute le formatage."
+
+    data = json.loads(content)
+    with open(path, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+
 
 
 if __name__ == "__main__":
-  pass
+    for path in [PATH_JOUEURS, PATH_TOURNOIS]:
+        initialisation_db(path)
+        format_db(path)
