744cdf7b0a3a768a98fb928f0e65e582e796b23b
Ceorl <dev.rocher.florian@gmail.com>
2025-06-24 16:54:14 +0200

    correction et formatage entrée utilisateur

--- views/view_chess.py
+++ views/view_chess.py
@@ -9,23 +9,23 @@ class View:
         print("Ajoutez nouveau joueur ")
         while True:
 
-            prenom = input("Prénom : ")
+            prenom = input("Prénom : ").capitalize()
             if prenom.isdigit():
                 print("Erreur sur le prénom")
                 prenom = input("Prénom : ").capitalize()
 
-            nom = input("Nom : ")
+            nom = input("Nom : ").capitalize()
             if nom.isdigit():
                 print("Erreur sur le nom")
                 nom = input("nom : ").capitalize()
 
-            date_naissance = input("Date de naissance (dd/mm/aa) : ")
-            pattern_date = r"^([0-2][0-9]|3[01])/([0][1-9]|1[0-2])/(\d{2})$"
+            date_naissance = input("Date de naissance (dd/mm/aaaa) : ")
+            pattern_date = r"^([0-2][0-9]|3[01])/([0][1-9]|1[0-2])/(\d{4})$"
             if not re.match(pattern_date, date_naissance):
                 print("Erreur date de naissance")
-                date_naissance = input("Date de naissance (dd/mm/aa) : ")
+                date_naissance = input("Date de naissance (dd/mm/aaaa) : ")
 
-            id_echec = input("ID nationnal d'échec : ")
+            id_echec = input("ID nationnal d'échec (AA12345): ")
             pattern_id = r'^[A-Z]{2}\d{5}$'
             if not re.match(pattern_id, id_echec):
                 print("Erreur ID nationnal d'échec (ex: JK45129)")
