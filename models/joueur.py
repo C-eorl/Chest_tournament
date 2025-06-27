ae9e9ba877aa175a35f64a3099b4a8095a79c1b3
Ceorl <dev.rocher.florian@gmail.com>
2025-06-27 15:43:32 +0200

    ajout m() => __repr__,

--- models/joueur.py
+++ models/joueur.py
@@ -1,6 +1,6 @@
 from datetime import date, datetime
-
 from tinydb import TinyDB, Query
+from utils import database
 
 
 class Joueur:
@@ -11,14 +11,26 @@ class Joueur:
         self.id_echec = id_echec
         self.score = score
 
+    def __iter__(self):
+        for key, value in self.__dict__.items():
+            if key == "date_naissance" and hasattr(value, "isoformat"): # pour compatibilité avec tiny DB
+                yield key, value.isoformat()
+            else:
+                yield key, value
+
+    def __repr__(self):
+        return f"Joueur: ({self.id_echec}: {self.nom} {self.prenom})"
+
+    def __str__(self):
+        return f"{self.id_echec}: {self.nom} {self.prenom}, né le {self.date_naissance.strftime("%d/%m/%Y")}"
+
     def save_joueur(self):
         """
         Accède à la base de donnée joueurs.json.
         Recherche si id du joueur existe, si False rajoute le Joueur à la base de donnée
         :return: None
         """
-        path = "../data/joueurs/joueurs.json"
-        db = TinyDB(path)
+        db = database.get_db_joueurs()
         JoueurQuery = Query()
         if not db.search(JoueurQuery.id_echec == self.id_echec):
             db.insert(dict(self))
@@ -26,19 +38,9 @@ class Joueur:
         else:
             print(str(self) + " existe déjà")
 
-    def __iter__(self):
-        for key, value in self.__dict__.items():
-            if key == "date_naissance" and hasattr(value, "isoformat"):
-                yield key, value.isoformat()
-            else:
-                yield key, value
-
-    def __str__(self):
-        return f"{self.id_echec}: {self.nom} {self.prenom}, né le {self.date_naissance}"
-
 
 if __name__ == "__main__":
-    bd = datetime.strptime("01/11/1945", "%d/%m/%Y").date()
+    bd = datetime.strptime("01/11/1945", "%d/%m/%Y")
     p = Joueur("test","test",bd, "QZ11122")
+    print(str(p))
 
-    print(dict(p))
