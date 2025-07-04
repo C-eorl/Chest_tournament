f98da26e1d9bdd261c730dc71354dab605a016d6
Ceorl <dev.rocher.florian@gmail.com>
2025-07-04 10:13:03 +0200

    point de lancement de l'application

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-
+from controllers.controller_interface import ControllerInterface
 
 if __name__ == "__main__":
-    pass
+    c = ControllerInterface()
+    while True:
+        c.main_menu()
+
+
