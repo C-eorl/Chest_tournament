1b22e4b0cf0f7b89e81d225d4ec421fd1e1d7034
Ceorl <dev.rocher.florian@gmail.com>
2025-07-07 15:00:25 +0200

    ajout methode to_dict() et from_dict()

--- models/player.py
+++ models/player.py
@@ -1,26 +1,21 @@
 from datetime import date, datetime
 
-
+# patern model façade
 class Player:
-    def __init__(self, name: str, firstname: str, birthday: date, id_chess: str, score:int = 0):
+    def __init__(self, name: str, firstname: str, birthdate: date, id_chess: str, score:int = 0):
         self.name = name
         self.firstname = firstname
-        self.birthday = birthday
+        self.birthdate = birthdate
         self.id_chess = id_chess
         self.score = score
 
-    def __iter__(self):
-        for key, value in self.__dict__.items():
-            if key == "birthday" and hasattr(value, "isoformat"): # pour compatibilité avec tiny DB
-                yield key, value.isoformat()
-            else:
-                yield key, value
+
 
     def __repr__(self):
         return f"Joueur: ({self.id_chess}: {self.name} {self.firstname})"
 
     def __str__(self):
-        return f"{self.id_chess}: {self.name} {self.firstname}, né le {self.birthday.strftime("%d/%m/%Y")}"
+        return f"{self.id_chess}: {self.name} {self.firstname}, né le {self.birthdate.strftime("%d/%m/%Y")}"
 
     def __lt__(self, other):
         """ dunder pour inférieur que """
@@ -32,8 +27,24 @@ class Player:
         if isinstance(other, Player):
             return self.score == other.score
 
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'firstname': self.firstname,
+            'birthdate': self.birthdate.strftime("%d/%m/%Y"),
+            'id_chess': self.id_chess
+        }
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            name = data["name"],
+            firstname = data["firstname"],
+            birthdate=datetime.strptime(data["birthdate"], "%Y/%m/%d").date(),
+            id_chess = data["id_chess"],
+        )
 
 if __name__ == "__main__":
     bd = datetime.strptime("01/11/1945", "%d/%m/%Y")
     p = Player("test", "test", bd, "QZ11122")
 
+    bd.strptime()
