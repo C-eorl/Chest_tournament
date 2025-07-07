9f8f6e975b40df9312de3e9a45290d18617f11d6
Ceorl <dev.rocher.florian@gmail.com>
2025-07-07 15:01:17 +0200

    correction pour add() et get_list_players()

--- models/player_DAO.py
+++ models/player_DAO.py
@@ -1,19 +1,20 @@
 from tinydb import Query
+from models.player import Player
 
 
 class PlayerRepository:
     def __init__(self, db):
+        # initialisation db ici
         self.db = db
         self.JoueurQuery = Query()
 
-    def add(self, data):
+    def add(self, player: Player ):
         """Ajoute un Joueur à la base de donnée"""
-        self.db.insert(dict(data))
+        self.db.insert(player.to_dict())
 
     def search(self, id_chess) -> dict | None:
         """ Recherche un joueur grâce à son id_chess"""
         return self.db.get(self.JoueurQuery.id_chess == id_chess)
-
 
     def update(self, id_chess: str, valeur: dict):
         """
@@ -26,4 +27,5 @@ class PlayerRepository:
 
     def get_list_players(self):
         """Renvoie la liste de tous les joueurs"""
-        return self.db.all()
+        players_data = self.db.all()
+        return [Player.from_dict(player) for player in players_data]
