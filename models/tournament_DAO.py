6ebbcc31f4e9491fef896527ed5f9eca0417b578
Ceorl <dev.rocher.florian@gmail.com>
2025-07-10 11:39:50 +0200

    finalisation TournamentRepository

--- models/tournament_DAO.py
+++ models/tournament_DAO.py
@@ -1,4 +1,5 @@
-from tinydb import Query
+from tinydb import Query, where
+from models.tournament import Tournament
 
 
 class TournamentRepository:
@@ -6,8 +7,29 @@ class TournamentRepository:
         self.db = db
         self.TournamentQuery = Query()
 
-    def add_db(self, data):
-        self.db.insert(dict(data))
+    def add(self, tournament : Tournament):
+        """Ajoute un Tournoi à la base de donnée"""
+        self.db.insert(tournament.to_dict())
 
-    def tournament_exist(self, name: str) -> bool:
+    def search(self, name: str) -> dict | None:
+        """Recherche un tournoi grâce à son name"""
         return self.db.search(self.TournamentQuery.name == name)
+
+    def search_is(self, statut: str) -> dict | None:
+        """Recherche un tournoi grâce à son name"""
+        return self.db.search(where("statut") == statut)
+
+
+    def update(self, name: str, valeur: dict):
+        """
+        Mets à jour les informations d'un tournoi
+        :param name: nom du tournoi
+        :param valeur: valeurs modifiées
+        :return: None
+        """
+        self.db.update(valeur, self.TournamentQuery.tournament_name == name)
+
+    def get_list_tournaments(self):
+        """Renvoie la liste de tous les tournois"""
+        tournaments_data = self.db.all()
+        return [Tournament.from_dict(tournament) for tournament in tournaments_data]
