b58a1578d4206cdc07ca8cc24b7c799bdb333f41
Ceorl <dev.rocher.florian@gmail.com>
2025-07-10 11:25:18 +0200

    rajout de champs possible lié au tournoi

--- utils/validation.py
+++ utils/validation.py
@@ -12,7 +12,7 @@ def validate_field(field_name: str, value: str) -> bool | str:
                 return f"Le {field_name} ne doit pas contenir de chiffres et ne peut pas être vide"
             return True
 
-        case "birthdate":
+        case "birthdate" | "end_date" | "start_date":
             pattern_date = r"^([0-2][0-9]|3[01])/([0][1-9]|1[0-2])/(\d{4})$"
             if not re.match(pattern_date, value):
                 return "Format invalide (dd/mm/aaaa)"
@@ -22,6 +22,26 @@ def validate_field(field_name: str, value: str) -> bool | str:
             pattern_id = r'^[A-Z]{2}\d{5}$'
             if not re.match(pattern_id, value):
                 return "Format invalide (ex: AA12345)"
+            return True
+
+        case "tournament_name":
+            if not value.strip():
+                return "Le nom du tournoi ne peut pas être vide"
+            return True
+
+        case "locality":
+            if not value.strip() or any(char.isdigit() for char in value):
+                return "La localité ne peut pas être vide ni contenir de chiffres"
+            return True
+
+        case "round_number":
+            if not value.isdigit():
+                return "Le nombre de rounds doit être un entier positif"
+            return True
+
+        case "description":
+            if not value.strip():
+                return "La description ne peut pas être vide"
             return True
 
         case _:
