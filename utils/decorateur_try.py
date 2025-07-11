32b2a9de7306cf0756767486630fde90cf3e7471
Ceorl <dev.rocher.florian@gmail.com>
2025-07-11 10:41:30 +0200

    ajout de la fonction decorator_try dans utils

---
+++ utils/decorateur_try.py
@@ -0,0 +1,15 @@
+from functools import wraps
+from time import sleep
+
+def decorator_try(fonction):
+    @wraps(fonction)
+    def wrapper(*args):
+        try:
+            return fonction(*args)
+        except TypeError as e:
+            print(f"[ERREUR] fonction:{fonction.__name__} - {e}")
+        except KeyError:
+            print("Annulation via Ctrl+c")
+
+
+    return wrapper
