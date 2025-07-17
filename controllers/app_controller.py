0eb85045ff0c8173dc153a4188d296fd706d6d1c
Ceorl <dev.rocher.florian@gmail.com>
2025-07-17 07:12:59 +0200

    création class AppController pour faire regrouper (médiateur) les controlleur

---
+++ controllers/app_controller.py
@@ -0,0 +1,21 @@
+
+from controllers.current_tournament_controller import ControllerCurrentTournament
+from controllers.player_controller import ControllerPlayer
+from controllers.tournament_controller import ControllerTournament
+from controllers.rapport_controller import ControllerRapport
+from controllers.main_menu_controller import MainMenuController
+
+
+class AppController:
+    def __init__(self):
+        self.controller_player = ControllerPlayer(self)
+        self.controller_tournament = ControllerTournament(self)
+        self.controller_rapport = ControllerRapport(self)
+        self.controller_current_tournament = ControllerCurrentTournament(self)
+        self.main_menu = MainMenuController(self)
+
+    def run(self):
+        self.main_menu.run()
+
+if __name__ == "__main__":
+    pass
