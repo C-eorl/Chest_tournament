3f8b6db193d46bbed31855473b860b7a00dd5be4
Ceorl <dev.rocher.florian@gmail.com>
2025-07-17 07:15:59 +0200

    ajout de getr_result

--- models/match.py
+++ models/match.py
@@ -1,25 +1,29 @@
 from datetime import datetime
-
-from player import Player
+from models.player import Player
 
 class Match:
     def __init__(self, player1: Player, player2: Player):
         self.player1 = player1
-        self.score1 = self.player1.score
         self.player2 = player2
-        self.score2 = self.player2.score
+        self.score1 = 0.0
+        self.score2 = 0.0
 
-    def __iter__(self):
-        for key, value in self.__dict__.items():
-            if isinstance(value, Player):
-                yield key, value
-            yield key, value
+    def __repr__(self):
+        return f"{self.player1} contre {self.player2}"
 
     def __str__(self):
-        return f"{self.player1} - SCORE: {self.player1.score}, {self.player2} - SCORE: {self.player2.score}"
+        return f"{self.player1} contre {self.player2}"
 
-    def result(self):
-        return ([self.player1, self.player1.score], [self.player2, self.player2.score])
+    def get_result(self, winner):
+        match winner:
+            case self.player1: self.score1 = 1
+            case self.player2: self.score2 = 1
+            case "Match nul":
+                self.score1 = 0.5
+                self.score2 = 0.5
+
+    def tuple_return(self):
+        return ([self.player1, self.score1], [self.player2, self.score2])
 
 if __name__ == "__main__":
     bd = datetime.strptime("01/11/1945", "%d/%m/%Y")
