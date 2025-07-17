3aad3f08e7001918074e97c880a87001f5ffcc3d
Ceorl <dev.rocher.florian@gmail.com>
2025-07-17 07:19:13 +0200

    Implementation class Round

---
+++ models/round.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+
+class Round:
+
+    def __init__(self, name: str):
+        self.list_match = []
+        self.name = name
+        self.date_time_start = None
+        self.date_time_end = None
+
+    def __str__(self):
+        return f"{self.name} avec comme resultat : {self.list_match}"
+
+    def start(self):
+        self.date_time_start = datetime.now()
+
+    def finish(self):
+        self.date_time_end = datetime.now()
+
+    def get_match_list(self):
+        return self.list_match
+if __name__ == "__main__":
+    pass
