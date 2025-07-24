f97d66653a8f08ee89903cd36e4371cf6b56f540
Ceorl <dev.rocher.florian@gmail.com>
2025-07-24 08:48:53 +0200

    suppression boucle while rename variable pour AppController()

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from controllers.app_controller import AppController
 
 if __name__ == "__main__":
-    c = AppController()
-    while True:
-        c.run()
+    app = AppController()
+    app.run()
