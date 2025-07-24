c0a65450f4e4a453725d365bda134acb4e66eb2f
Ceorl <dev.rocher.florian@gmail.com>
2025-07-24 08:52:42 +0200

    refact. boucle run() implementation players_in_tournament

--- controllers/rapport_controller.py
+++ controllers/rapport_controller.py
@@ -1,5 +1,4 @@
 from views.view import View
-from utils.exit_menu import retour
 
 
 class ControllerRapport:
@@ -7,16 +6,31 @@ class ControllerRapport:
         self.app = app_controller
         self.view = View()
         self.controller_player = self.app.controller_player
+        self.controller_tournament = self.app.controller_tournament
 
     def run(self):
         """Menu rapport"""
-        title = ("Gestion des rapports\n"
-                 "Choisissez une option:")
-        option = {
+        title = "Gestion des rapports - Choisissez une option:\n"
+        options = {
             "Liste de tous les joueurs": self.controller_player.list_players,
-            "Liste de tous les tournois": NotImplemented,
-            "Liste des joueurs du tournoi par ordre alphabétique": NotImplemented,
+            "Liste de tous les tournois": self.controller_tournament.list_tournaments,
+            "Liste des joueurs du tournoi par ordre alphabétique": self.players_in_tournament,
             "Exporter les rapports": NotImplemented,
-            "Retour": retour
+            "Retour": None
         }
-        self.view.menu(title, option)
+        while True:
+            choice = self.view.menu(title, options)
+            if choice is None:
+                break
+            choice()
+
+    def players_in_tournament(self):
+
+        list_tournament = self.controller_tournament.repo_tournament.get_list_tournaments()
+        if list_tournament:
+            tournament = self.view.display_selected_data(list_tournament)
+            title_table= f"Liste des joueurs pour le tournoi {tournament.tournament_name}"
+            fields = ["id_chess","name", "firstname", "birthdate"]
+            self.view.display_list_data(tournament.list_participant, title_table, fields)
+        else:
+            self.view.display_message("Aucun tournoi dans la base de donnée")
