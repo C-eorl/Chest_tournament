6553857e2bcc43640f595c142ae22a9ab2851529
Ceorl <dev.rocher.florian@gmail.com>
2025-07-24 08:53:25 +0200

    ajout parametre lone_player serialization & deserialization

--- models/round.py
+++ models/round.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 
+from models.match import Match
+from models.player import Player
+
 
 class Round:
 
@@ -8,9 +11,31 @@ class Round:
         self.name = name
         self.date_time_start = None
         self.date_time_end = None
+        self.lone_player = None
 
     def __str__(self):
         return f"{self.name} avec comme resultat : {self.list_match}"
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "list_match": [match.to_dict() for match in self.list_match],
+            "lone_player": self.lone_player.to_dict() if self.lone_player else None,
+            "start_date": self.date_time_start.strftime("%d/%m/%Y %H:%M:%S") if self.date_time_start else None,
+            "end_date": self.date_time_end.strftime("%d/%m/%Y %H:%M:%S") if self.date_time_end else None
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        round = cls(data["name"])
+        round.list_match = [Match.from_dict(m) for m in data.get("list_match", [])]
+        lone_player = data.get("lone_player")
+        round.lone_player = Player.from_dict(lone_player) if lone_player else None
+        if data.get("start_date"):
+            round.date_time_start = datetime.strptime(data["start_date"], "%d/%m/%Y %H:%M:%S")
+        if data.get("end_date"):
+            round.date_time_end = datetime.strptime(data["end_date"], "%d/%m/%Y %H:%M:%S")
+        return round
 
     def start(self):
         """défini le debut du round"""
