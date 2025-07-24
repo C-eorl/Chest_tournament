8b4d8a71c3afd6371fef281452009a05a266e395
Ceorl <dev.rocher.florian@gmail.com>
2025-07-24 08:49:27 +0200

    refact. boucle run()

--- controllers/main_menu_controller.py
+++ controllers/main_menu_controller.py
@@ -9,16 +9,20 @@ class MainMenuController:
     @decorator_try
     def run(self):
         """Affiche le menu principal"""
-        title = "Chess Tournament manager\nChoisissez une option :"
+        title = "Chess Tournament manager - Choisissez une option :\n"
         options = {
             "Joueurs": self.app.controller_player.run,
             "Tournois": self.app.controller_tournament.run,
             "Continuez un tournoi existant": self.app.controller_current_tournament.run,
             "Rapports": self.app.controller_rapport.run,
-            "Quittez": exit
+            "Quittez": None
         }
 
         exist_current = self.app.controller_current_tournament.list_current_tournament()
         if not exist_current:
             options.pop("Continuez un tournoi existant")
-        self.view.menu(title, options)
+        while True:
+            choice = self.view.menu(title, options)
+            if choice is None:
+                break
+            choice()
