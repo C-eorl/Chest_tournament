bdb9e387dd8287fbffb3904ae50653f9de0f0a41
Ceorl <dev.rocher.florian@gmail.com>
2025-07-24 08:58:08 +0200

    suppression de modification date ajout style table rich implementaion display_classement

--- views/view_tournament.py
+++ views/view_tournament.py
@@ -27,10 +27,7 @@ class ViewTournament(View):
         start_date = questionary.text("Date de début du tournoi: ", validate=lambda text: validate_field("start_date", text)).ask()
         end_date = questionary.text("Date de fin du tournoi: ", validate=lambda text: validate_field("end_date", text)).ask()
 
-        start_date = datetime.strptime(start_date, "%d/%m/%Y").date()
-        end_date = datetime.strptime(end_date, "%d/%m/%Y").date()
-
-        return [name, locality, description, start_date, end_date, int(round_number)]
+        return [name, locality, description, start_date, end_date, round_number]
 
     def display_selected_tournament(self, list_current_tournament) -> Tournament:
         """
@@ -51,9 +48,9 @@ class ViewTournament(View):
         return target
 
     def display_tournament(self, tournament):
-        table = Table(show_header=True, header_style="bold magenta")
+        table = Table(show_header=False, header_style="bold magenta")
         table.add_column("Champ")
-        table.add_column("Valeur", style="bold cyan")
+        table.add_column("Valeur", style="bold cyan", width= 25)
 
         table.add_row("Nom", tournament.tournament_name)
         table.add_row("Lieu", tournament.locality)
@@ -67,29 +64,56 @@ class ViewTournament(View):
         table_participant.add_column("Participant")
         table_participant.add_column("Score")
         for participant in tournament.list_participant:
-            table_participant.add_row( str(participant), "score")
+            score = tournament.classement.get(participant, 0)
+            table_participant.add_row( str(participant), str(score))
 
+        panel = Panel(
+            table,
+            title=f"[bold #29a7ab]Tournoi : {tournament.tournament_name}",
+            border_style="#29a7ab",
+            expand= False,
 
-
-        panel = Panel(table, title=f"[bold green]Tournoi : {tournament.tournament_name}", border_style="green")
-        panel_participant = Panel(table_participant)
+        )
+        panel_participant = Panel(
+            table_participant,
+            title="[bold #29a7ab]Classement des joueurs",
+            border_style="#29a7ab",
+            expand= False
+        )
         self.console.print(panel)
         self.console.print(panel_participant)
 
     def display_round(self, round):
         console = Console()
 
-        title = Text(f"🏆 {round.name}", style="bold blue", justify="center")
-        console.print(Panel(title, style="blue", padding=(1, 2)))
+        title = Text(f"🏆 {round.name}", style="bold #29a7ab", justify="center")
+        console.print(Panel(title, style="#29a7ab", padding=(1, 2)))
 
-        table = Table(title="📋 Liste des Matchs", box=box.ROUNDED, show_header=True)
-        table.add_column("Joueur 1", style="magenta", width=20)
-        table.add_column("Score", justify="center", style="yellow", width=10)
-        table.add_column("Joueur 2", style="magenta", width=20)
+        table = Table(title="📋 Liste des Matchs",
+                      box=box.ROUNDED,
+                      show_header=True,
+                      expand=True)
+        table.add_column("Joueur 1", style="#29a7ab", width=20)
+        table.add_column("Score", justify="center", style="#ed7f07 bold", width=10)
+        table.add_column("Joueur 2", style="#29a7ab", width=20)
 
         list_match = round.get_match_list()
-
+        lone_player = round.lone_player
         for match in list_match:
                 score_text = f"{match.score1} - {match.score2}"
-                table.add_row(str(match.player1), score_text, str(match.player2))
+                table.add_row(match.player1.simple_str(), score_text, match.player2.simple_str())
+                table.add_row("","","")
+        if lone_player:
+            table.add_row(lone_player.simple_str(), "1.0 - 0.0", " / ")
+        console.print(table)
+
+    def display_classement(self, classement):
+        console = Console()
+        table = Table(
+            title="Classement",
+        )
+        table.add_column("Joueur")
+        table.add_column("Score")
+        for player, score in classement.items():
+            table.add_row(player.simple_str(), str(score))
         console.print(table)
