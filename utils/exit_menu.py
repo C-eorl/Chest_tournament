59b4fa1d626961f563b08f35b651af152fcfa3ad
Ceorl <dev.rocher.florian@gmail.com>
2025-07-24 10:29:33 +0200

    suppression

--- utils/exit_menu.py
+++
@@ -1,5 +0,0 @@
-def retour():
-    """Appel interne de la fonction pour affiche le menu principal"""
-    from controllers.app_controller import AppController
-    controller = AppController()
-    return controller.run()
