8aefaf4f599b40a64814138bf2ed7226a683432c
Ceorl <dev.rocher.florian@gmail.com>
2025-07-24 08:50:28 +0200

    serialization & deserialization Match changement affichage __str__

--- models/match.py
+++ models/match.py
@@ -12,12 +12,30 @@ class Match:
         return f"{self.player1} contre {self.player2}"
 
     def __str__(self):
-        return f"{self.player1} contre {self.player2}"
+        return f"{self.player1.simple_str()} contre {self.player2.simple_str()}"
+
+    def to_dict(self):
+        return {
+            "player1": self.player1.to_dict(),
+            "player2": self.player2.to_dict(),
+            "score1": self.score1,
+            "score2": self.score2
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        match= cls(
+            player1=Player.from_dict(data["player1"]),
+            player2=Player.from_dict(data["player2"])
+        )
+        match.score1 = data.get("score1", 0.0)
+        match.score2 = data.get("score2", 0.0)
+        return match
 
     def get_result(self, winner):
         match winner:
-            case self.player1: self.score1 = 1
-            case self.player2: self.score2 = 1
+            case self.player1: self.score1 = 1.0
+            case self.player2: self.score2 = 1.0
             case "Match nul":
                 self.score1 = 0.5
                 self.score2 = 0.5
