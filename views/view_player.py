b8641ebbdfed7a54109b3cab6352ece59e9c2b77
Ceorl <dev.rocher.florian@gmail.com>
2025-07-24 08:56:45 +0200

    ajout style (icone question)

--- views/view_player.py
+++ views/view_player.py
@@ -5,16 +5,28 @@ from views.view import View
 
 class ViewPlayer(View):
 
-
     def input_player(self):
 
-        firstname = questionary.text("Prénom :", validate=lambda text: validate_field("firstname", text)).ask()
+        firstname = questionary.text("Prénom :",
+                                     validate=lambda text: validate_field("firstname", text),
+                                     qmark="✦",
+                                     ).ask()
         if firstname:
             firstname = firstname.capitalize()
-        name = questionary.text("Nom :", validate=lambda text: validate_field("name", text)).ask()
+        name = questionary.text("Nom :",
+                                validate=lambda text: validate_field("name", text),
+                                qmark="✦",
+                                ).ask()
         if name:
             name = name.capitalize()
-        birthdate = questionary.text("Date de naissance (dd/mm/aaaa) :", validate=lambda text: validate_field("birthdate", text)).ask()
-        id_chess = questionary.text("ID national d'échec (ex: AA12345) :", validate=lambda text: validate_field("id_chess", text)).ask()
+        birthdate = questionary.text(
+            "Date de naissance (dd/mm/aaaa) :",
+            validate=lambda text: validate_field("birthdate",text),
+            qmark="✦",
+        ).ask()
+        id_chess = questionary.text("ID national d'échec (ex: AA12345) :",
+                                    validate=lambda text: validate_field("id_chess", text),
+                                    qmark="✦",
+                                    ).ask()
 
         return [name, firstname, birthdate, id_chess]
