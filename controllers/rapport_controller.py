32a22bce1c066dbde4615cf7d7860b85738e1ccc
Ceorl <dev.rocher.florian@gmail.com>
2025-07-30 05:55:03 +0200

    Ajout fonction liste Round/match

--- controllers/rapport_controller.py
+++ controllers/rapport_controller.py
@@ -15,7 +15,8 @@ class ControllerRapport:
             "Liste de tous les joueurs": self.controller_player.list_players,
             "Liste de tous les tournois": self.controller_tournament.list_tournaments,
             "Liste des joueurs du tournoi par ordre alphabétique": self.players_in_tournament,
-            "Exporter les rapports": NotImplemented,
+            "Liste des Rounds et Matchs d'un tournoi": self.rounds_match_in_tournament,
+            # "Exporter les rapports": NotImplemented,
             "Retour": None
         }
         while True:
@@ -35,5 +36,15 @@ class ControllerRapport:
             fields = ["id_chess", "name", "firstname", "birthdate"]
             sorted_list = sorted(tournament.list_participant, key=lambda player: player.name)
             self.view.display_list_data(sorted_list, title_table, fields)
+        else:
+            self.view.display_message("Aucun tournoi dans la base de donnée")
+
+    def rounds_match_in_tournament(self):
+        list_tournament = self.controller_tournament.repo_tournament.get_list_tournaments()
+        if list_tournament:
+            tournament = self.view.display_selected_data(list_tournament)
+            for round in tournament.rounds:
+                view = self.controller_tournament.view
+                view.display_round(round)
         else:
             self.view.display_message("Aucun tournoi dans la base de donnée")
