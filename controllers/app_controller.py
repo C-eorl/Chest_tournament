655f307a6d6255f34534379fb18c6a9c28de8244
Ceorl <dev.rocher.florian@gmail.com>
2025-07-30 06:37:36 +0200

    initialisation dossier DB - update

--- controllers/app_controller.py
+++ controllers/app_controller.py
@@ -3,7 +3,6 @@ from controllers.player_controller import ControllerPlayer
 from controllers.tournament_controller import ControllerTournament
 from controllers.rapport_controller import ControllerRapport
 from controllers.main_menu_controller import MainMenuController
-from utils.database import initialise_data_directories
 
 
 class AppController:
@@ -16,5 +15,4 @@ class AppController:
 
     def run(self):
         """Lance la fonction de départ du memu principal et initialise les dossiers DB"""
-        initialise_data_directories()
         self.main_menu.run()
